b6e2242167c2960b2f9fa267b3b3bef89361c015
Vincent Liussyaputra <liusvinv@gmail.com>
2023-06-09 14:58:36 +0700

    Add layout to cart page and refactor CartItemDisplayer

--- src/page/Cart.jsx
+++ src/page/Cart.jsx
@@ -6,26 +6,34 @@ const Cart = () => {
     const dispatch = useDispatch();
     return (
         <div className="cart">
+            <div className="cart-listing">
                 {cart && cart.map((data)=>{
                     return (
-                        <div key={data.productId}>
-                            <h2>Product {data.productId}</h2>
-                            {data.qty.map((variantList)=>(
-                                <div>
-                                    <h3>Variant {variantList.variant}</h3>
-                                    {/* <p>Size {variantList.size}</p> */}
-                                    <p>Amount: {variantList.amount}</p>
-                                </div>
-                            ))}
-                            <button onClick={()=>{dispatch(cartActions.remove(data.productId))}}>Remove from cart</button>
-                        </div>
+                      <CartItemDisplayer   data={data} />
                     )
                 })}
+                {cart.length === 0 && <p>There's no item in your cart</p>}
+            </div>
+            <div className="cart-action">
+                This is the checkout stuff
+            </div>
         </div>
     ) 
 }
 
-function CartItemDisplayer(){
+function CartItemDisplayer({data}) {
 
+    const dispatch = useDispatch();
+    return (
+        <div key={data.productId}>
+            <h2>Product {data.productId}</h2>
+            {data.qty.map(variantList => <div>
+                    <h3>Variant {variantList.variant}</h3>
+                    {/* <p>Size {variantList.size}</p> */}
+                    <p>Amount: {variantList.amount}</p>
+                                    </div>
+            )}<button onClick={() => {dispatch(cartActions.remove(data.productId));}}>Remove from cart</button>
+        </div>
+    );
 }
-export default Cart
+    export default Cart
