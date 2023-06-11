5e1f85d55246a5ddf9408b06188bec7ce42008e0
Vincent Liussyaputra <liusvinv@gmail.com>
2023-06-11 14:23:26 +0700

    Add layout styling to the product detail page

--- src/page/DetailedProduct.jsx
+++ src/page/DetailedProduct.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { cartActions } from "../redux/features/cart/cartSlice";
 
 function DetailedProduct(){
-    const [variant,setVariant] = useState(1);
+    const [variant,setVariant] = useState(0);
     const [qty,setQty] = useState(1);
 
     const dispatch = useDispatch();
@@ -15,7 +15,7 @@ function DetailedProduct(){
     function handleAdd(){
         try{
             const quantity = parseInt(qty);
-            const cartItem = {id:product.id,qty:quantity,variant:Math.floor(Math.random()*3)}
+            const cartItem = {id:product.id,qty:quantity,variant:variant}
             dispatch(cartActions.add(cartItem));
         }catch(err){
             alert('Please enter a number for the quantity' + err);
@@ -23,32 +23,38 @@ function DetailedProduct(){
     }
 
     return (
-        <div className="product">
-            <div>
-                <img className="prod-img" src={product.imageSrc || 'https://lzd-img-global.slatic.net/g/p/a1fa48a04c8c30705c9c2ea1bbf6d3d0.jpg_720x720q80.jpg_.webp'} alt="" />
-            </div>
-            <div className="prod-data">
-                <h2 className="name">{product.name}</h2>
-                <p className="category">{product.category}</p>
-                <p className="desc">{product.description}</p>
-                <div className="variants">
-                    {product.variants.map((variant,index)=>{
-                        return <div key={index}>
-                            <h4>Variant {index + 1}</h4>
-                            <p>{variant.color}</p>
-                            <p>{variant.size}</p>
-                        </div>
-                    })}
+        <div className="product-page">
+            <section className="product-detail">
+                <div>
+                    <img className="prod-img" src={product.imageSrc || 'https://lzd-img-global.slatic.net/g/p/a1fa48a04c8c30705c9c2ea1bbf6d3d0.jpg_720x720q80.jpg_.webp'} alt="" />
                 </div>
-               
-            </div>
-            <form className="prod-footer" onSubmit={(e)=>{e.preventDefault()}}>
-                    <p className="price">${product.price}</p>
-                    {/* <label htmlFor="qty">Qty:</label> */}
-                    {/* <input type="number" onChange={(e)=>setQty(e.target.value)} value={qty} min={1} max={99} name="qty" /> */}
-                    <button className="btn cart-btn" onClick={handleAdd}>Add to cart</button>
-            </form>
-            {/* <p>{product.variants[0].size}</p> */}
+                <div className="prod-data">
+                    <h2 className="name">{product.name}</h2>
+                    <p className="category">{product.category}</p>
+                    <p className="desc">{product.description}</p>
+                </div>
+                <form className="prod-action" onSubmit={(e)=>{e.preventDefault()}}>
+                    <div className="floating">
+                        <div className="variant-field">
+                            <p>Variant:</p>
+                            <div className="variants">
+                                    {product.variants.map((variantData,index)=>{
+                                        return <button className={`variant ${index == variant && 'selected'}`} onClick={()=>setVariant(index)} key={index}>
+                                            <p className="type">{variantData.color}</p>
+                                            <p className="size">{variantData.size}</p>
+                                        </button>
+                                    })}
+                            </div>
+                        </div>
+                        
+                        <div className="pricing-field">
+                            <h2 className="heading">Total</h2>
+                            <p className="price">${product.price}</p>
+                            <button className="btn cart-btn" onClick={handleAdd}>Add to cart</button>
+                        </div>
+                    </div>
+                </form>
+            </section>
         </div>
     )
 }
