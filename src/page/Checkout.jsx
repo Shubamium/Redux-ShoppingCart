042489c82f758a3eb5662629a90059aac6ac109f
Vincent Liussyaputra <liusvinv@gmail.com>
2023-06-12 18:22:35 +0700

    Add price total in order summary

--- src/page/Checkout.jsx
+++ src/page/Checkout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
 function Checkout() {
@@ -20,38 +20,45 @@ function Checkout() {
 function OrderSummary(){
     const carts = useSelector((state)=>state.cart);
     const productData = useSelector((state)=>state.products);
-
     const getProductData = (prodId) =>{
         const findIndex = productData.findIndex((prod) => prod.id === prodId)
         return productData[findIndex] || null;
     }
     function renderOrderSummary(){
         let toRender = [];
+        let totalPrice = 0;
         // For each cart
         carts.forEach(cart => {
             // Add the cart data and variant to render list
             const thisProduct = getProductData(cart.productId);
-            const data = cart.qty.map((cartQty,index)=>(
-                <tr key={index} className='order-summary'>
-                    <td>
-                          { thisProduct && thisProduct.name}
-                    </td>
-                    <td>
-                         <span>{thisProduct.variants[cartQty.variant].color}</span> - <span>{thisProduct.variants[cartQty.variant].size}</span> 
-                    </td>
-                    <td>
-                        {cartQty.amount}
-                    </td>
-                    <td>
-                       ${thisProduct && (thisProduct.price*cartQty.amount).toFixed(2)}
-                    </td>
-                </tr>
-            ));
+            const data = cart.qty.map((cartQty,index)=> {
+                let price = (thisProduct.price*cartQty.amount).toFixed(2);
+                totalPrice += parseInt(price);
+                return (
+                    <tr key={index} className='order-summary'>
+                        <td>
+                              { thisProduct && thisProduct.name}
+                        </td>
+                        <td>
+                             <span>{thisProduct.variants[cartQty.variant].color}</span> - <span>{thisProduct.variants[cartQty.variant].size}</span> 
+                        </td>
+                        <td>
+                            {cartQty.amount}
+                        </td>
+                        <td>
+                           ${thisProduct && price}
+                        </td>
+                    </tr>
+                )
+            });
 
             toRender.push(...data);
         })                
-        return toRender;
+       
+        return {toRender,totalPrice};
     }
+
+    const {totalPrice,toRender} = renderOrderSummary()
     return(
         <table className='payment-info' border='2px'>
                 <thead>
@@ -71,7 +78,11 @@ function OrderSummary(){
                         Price
                     </th>
                 </tr>
-                {renderOrderSummary()} 
+                {toRender} 
+                <tr>
+                    <td colSpan={2} align='right'>Price Total:</td>
+                    <td colSpan={2}> ${totalPrice} </td>
+                </tr>
         </table>
     )
 }
